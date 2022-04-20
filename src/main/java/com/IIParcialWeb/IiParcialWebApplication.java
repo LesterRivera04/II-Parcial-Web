5c5286c6c19442ad37dd63a17b3d863b98441607
Lester <>
2022-04-19 20:43:41 -0600

    crear el II Parcial Progra Web

---
+++ src/main/java/com/IIParcialWeb/IiParcialWebApplication.java
@@ -0,0 +1,13 @@
+package com.IIParcialWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiParcialWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiParcialWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IIParcialWeb/IiParcialWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IIParcialWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiParcialWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
